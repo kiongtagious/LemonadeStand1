cb8b3000467dd25091b8b5df6264014376acf570
kiongtagious <kionglao20@gmai.com>
2017-08-11 08:59:33 -0500

    Created User Interface Class and added opening lines

---
+++ ConsoleApplication1/ConsoleApplication1/UserInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class UserInterface
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to The Lemonade Stand!");
+            Console.ReadKey();
+            Console.WriteLine("Glad you came along.... partner!");
+            Console.ReadKey();
+            Console.WriteLine("Here's how it works... You get $10 to start off with to purchase your first few items for your lemonade stand.");
+            Console.ReadKey();
+            Console.WriteLine("Your goal is to make a profit in return BUT... it's not that easy.");
+            Console.ReadKey();
+            Console.WriteLine("Whether you make a profit will depend on other factors such as weather and price. ");
+            Console.ReadKey();
+            Console.WriteLine("Think you got what it takes? Ready to make some gwala gwala? Well then....");
+            Console.ReadKey();
+            Console.WriteLine("Let's get ready to HUSTLLLLLLLLEEE! - Michael Buffer(Boxing Ring Announcer)");
+
+        }
+    }
+}
